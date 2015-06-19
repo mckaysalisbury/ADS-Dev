ac0d79bb8548e980cbf8270a5a3e92cd5ba948dd
lllpsi <leoluis@l33t.local>
2015-06-19 15:29:42 -0400

    Hooking up angular modules

--- public/layout/shell.controller.js
+++ public/layout/shell.controller.js
@@ -5,29 +5,12 @@
         .module([])
         .controller('ShellController', ShellController);
 
-    ShellController.$inject = ['$rootScope', '$timeout', 'config', 'logger'];
+    ShellController.$inject = ['$rootScope', '$timeout'];
     /* @ngInject */
-    function ShellController($rootScope, $timeout, config, logger) {
+    function ShellController($rootScope, $timeout) {
         var vm = this;
         vm.busyMessage = 'Please wait ...';
         vm.isBusy = true;
-        $rootScope.showSplash = true;
-        vm.navline = {
-            title: config.appTitle,
-        };
-
-        activate();
-
-        function activate() {
-            logger.success(config.appTitle + ' loaded!', null);
-            hideSplash();
-        }
-
-        function hideSplash() {
-            //Force a 1 second delay so we can see the splash.
-            $timeout(function() {
-                $rootScope.showSplash = false;
-            }, 1000);
-        }
+        vm.name = 'Leo lololz';
     }
 })();
