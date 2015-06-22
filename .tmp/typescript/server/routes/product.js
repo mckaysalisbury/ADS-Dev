79b3400d25fadfb5fe7441077bcc31a4f5c290da
lllpsi <leoluis@l33t.local>
2015-06-22 17:21:45 -0400

    Adding git ignore

--- .tmp/typescript/server/routes/product.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var path = require('path');
-var router = express.Router();
-router.get('/:productId', function (req, res, next) {
-    res.sendFile(path.join(__dirname, "../views/product.html"));
-});
-module.exports = router;
-//# sourceMappingURL=product.js.map
