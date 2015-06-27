156c46f2717de03b44b0d26d42fd941c03b88c62
lllpsi <leoluis@l33t.home>
2015-06-27 14:45:08 -0400

    Fixing icon and lint

--- src/client/app/search-by-purpose/search-by-purpose.module.js
+++ src/client/app/search-by-purpose/search-by-purpose.module.js
@@ -1,5 +1,5 @@
 (function () {
     'use strict';
 
-    angular.module('app.search-by-purpose', ['app.core','app.search-form']);
+    angular.module('app.search-by-purpose', ['app.core', 'app.search-form']);
 })();
