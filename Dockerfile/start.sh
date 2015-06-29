2265a365cda2560f843c0a1462ca74d105cf0623
RickyJenkins <vjenkins@pyramidsystems.com>
2015-06-29 14:23:39 -0400

    Update start.sh

--- Dockerfile/start.sh
+++ Dockerfile/start.sh
@@ -2,10 +2,10 @@ cd /tmp
 
 rm -rf ADS-Dev; true
 
-git clone https://github.com/PyramidSystemsInc/ADS-Dev.git -b main
+git clone https://github.com/PyramidSystemsInc/ADS-Dev.git -b master
 
 cd ADS-Dev
 
 npm install gulp -g
-
+npm install
 gulp serve-build
