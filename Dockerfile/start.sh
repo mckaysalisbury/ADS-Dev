7fc443890357e8d01ef9d2671e37f8646ed1202a
Ricky Jenkins <vjenkins@pyramidsystems.com>
2015-06-29 17:36:48 -0400

    updated as part o f the container build

--- Dockerfile/start.sh
+++ Dockerfile/start.sh
@@ -1,9 +1,13 @@
+cd /tmp
+
 rm -rf ADS-Dev; true
 
-git clone https://github.com/PyramidSystemsInc/ADS-Dev.git -b master
+git clone https://github.com/PyramidSystemsInc/ADS-Dev.git
 
 cd ADS-Dev
 
 npm install
+
+bower install
 
 npm start
