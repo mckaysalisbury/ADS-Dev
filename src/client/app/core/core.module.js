35e6a9726a60df9e49f3691842e06a3b0afbbd68
lllpsi <leoluis@l33t.home>
2015-07-07 02:37:53 -0400

    Removing ngicon

--- src/client/app/core/core.module.js
+++ src/client/app/core/core.module.js
@@ -5,7 +5,7 @@
         .module('app.core', [
         'ngAnimate', 'ngSanitize',
         'blocks.exception', 'blocks.logger', 'blocks.router',
-        'ui.router', 'ngMaterial', 'ngMdIcons']).config(function ($mdThemingProvider) {
+        'ui.router', 'ngMaterial']).config(function ($mdThemingProvider) {
         $mdThemingProvider.theme('default')
             .primaryPalette('blue')
             .accentPalette('pink');
